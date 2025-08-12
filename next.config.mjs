11d12908832f2b47d09588ec9e9813c5377c5886
Sparsh V Patel <158298418+Sparsh12342@users.noreply.github.com>
2025-08-11 21:59:53 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.pexels.com' },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "images.pexels.com" }],
   },
 };
-module.exports = nextConfig;
+export default nextConfig;
